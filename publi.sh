0c0364fe6be78d4272a229aa6f785ba97e4bd98f
Destan Sarpkaya <destan@dorukdestan.com>
2014-06-16 17:25:13 +0000

    update publish script to remove old files

--- publi.sh
+++ publi.sh
@@ -2,6 +2,8 @@ git checkout jekyll
 jekyll build
 cp .gitignore ../static/
 git checkout master
+# remove all files and directories except .git
+find . | grep -v ".git" | xargs rm -rf
 cp -r ../static/* ./
 rm -rf .sass-cache/
 rm -rf _site/
