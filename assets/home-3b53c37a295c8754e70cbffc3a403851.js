43dcd517ee69a673b38f9ccc6662fa934046506b
Destan Sarpkaya <destan@dorukdestan.com>
2014-08-03 20:19:55 +0000

    update

---
+++ assets/home-3b53c37a295c8754e70cbffc3a403851.js
@@ -0,0 +1,1 @@
+$(function(){$(".timeline-desc, .for-tooltip, .timeline-date").each(function(){var t=$(this);t.attr("data-original-title")&&t.tooltip()}),$("#moreOfMeToggler").click(function(){$("#moreOfMe").toggle()})});
