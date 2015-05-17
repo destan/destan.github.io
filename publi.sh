d71309d6d9b527e4fa53629e5ae62e9509d493a1
Destan Sarpkaya <destan@dorukdestan.com>
2015-05-17 23:19:05 +0300

    fix publish script

--- publi.sh
+++ publi.sh
@@ -1,6 +1,6 @@
 git checkout jekyll
-rm _config.yml
-cp _config.yml.prod _config.yml 
+cp _config.yml.prod _config.yml
+commit -am 'restore prod config'
 jekyll build
 cp .gitignore ../static/
 git checkout master
